98877060d1731f996c51ec893cf41fbf725f82a3
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-11 03:28:50 -0300

    aula: arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,69 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
-
   // podemos criar variáveis, o código gerado é javascript
   // código gerado dentro do return entre chaves {} também é javascript
   const random = Math.random();
   const ativo = false;
 
   // podemos misturar com html
-  const titulo = <h1>App React Curso</h1>
+  const titulo = <h1>App React Curso</h1>;
 
-  // funcoes recebendo parâmetros 
+  // funcoes recebendo parâmetros
   function curso(nome) {
     return "Curso completo de " + nome;
   }
 
-  const carro ={
-    marca :'Ford',
-    rodas : 4,
+  const carro = {
+    marca: "Ford",
+    rodas: 4,
   };
 
   // CSS
-  const estiloP ={
-    color : 'blue',
+  const estiloP = {
+    color: "blue",
   };
+
+  // trabalhando com array
+  const filmes = ["Before Sunrise", "Before Sunset", "Before Midnight"];
+
+  const livros = [
+    { nome: "A game of thrones", ano: 1996 },
+    { nome: "A clash of kings", ano: 1998 },
+    { nome: "A storm of swords", ano: 2000 },
+  ];
 
   return (
     // necessário abrir de fechar tag se não for uma div <></>
     // seria a mesma coisa que colocar <React.Fragment></React.Fragment> de forma reduzida
-    <> 
+    <>
       {titulo}
-      <p style={estiloP} className={ativo ? 'ativo' : 'inativo'}>Random: {random}</p>
-      <p style={estiloP}>{curso('React')}</p>
-      <p>{new Date().toISOString()}</p> 
-      <p>{carro.marca} com {carro.rodas} rodas</p>
+      <p style={estiloP} className={ativo ? "ativo" : "inativo"}>
+        Random: {random}
+      </p>
+      <p style={estiloP}>{curso("React")}</p>
+      <p>{new Date().toISOString()}</p>
+      <p>
+        {carro.marca} com {carro.rodas} rodas
+      </p>
+
+      {/* trabalhando com lista */}
+      <ul>
+        {filmes.map((filme) => (
+          <li key={filme}> {filme}</li>
+        ))}
+      </ul>
+      <hr />
+      <ul>
+        {livros
+          .filter((livro) => livro.ano >= 1998)
+          // desestruturando, seria mesma coisa que passar 'livro' e depois livro.nome e livro.ano
+          .map(({ nome, ano }) => (
+            <li key={nome}>
+              {nome}, {ano}
+            </li>
+          ))}
+      </ul>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Exercicio from './Exercicio.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Exercicio from "./Exercicio.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <>
+createRoot(document.getElementById("root")).render(
+  <>
     <App />
-    <Exercicio/>
-    </>
-)
+    <Exercicio />
+  </>
+);
